a560db585c94d1565285f2a89d0b6e5b540718cb
MikkoJaakola <mikko.jaakola@hotmail.fi>
2023-01-21 17:10:11 +0200

    Created functionality for calculating prime factors from user given input. Program now calculates all prime factors of user given number, compiles a list of them and displays this list as output.

--- PrimeNumberCalculator.py
+++ PrimeNumberCalculator.py
@@ -1,6 +1,25 @@
-user_number = input("Give me number:")
+print("With this program you can calculate the prime factors of any given number")
 
-print(user_number)
+
+# Store user input in a variable
+user_number = int(input("Give me number:"))
+
+new_number = user_number + 69
+
+# calculate prime factors of user given number
+def calc_prime_fact(x):
+    prime_factors = []
+    i = 2
+    while i <= x:
+        if x%i == 0:
+            prime_factors.append(i)
+            x = x/i
+        else:
+            i += 1
+    return prime_factors
+    
+
+print(calc_prime_fact(user_number))
 
 
 
