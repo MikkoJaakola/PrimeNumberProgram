0f3747023141b199235f60391097057c26f08fac
MikkoJaakola <mikko.jaakola@hotmail.fi>
2023-01-22 16:50:58 +0200

    Created functionality for showing execution time in output

--- PrimeNumberCalculator.py
+++ PrimeNumberCalculator.py
@@ -1,4 +1,6 @@
 import sqlite3 as db
+import time
+
 
 # connect to database file to store already searched numbers, create the file if it doesn't exist
 con = db.connect('Searched_numbers.db')
@@ -12,7 +14,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS factors (
             factors text
  )""")
 
-
+print("*****")
 print("With this program you can calculate the prime factors of any given number")
 
 
@@ -39,10 +41,14 @@ def cut_duplicates(list):
             refined_list.append(i)
     return refined_list
 
+# starting time of fetching prime factors
+start = time.time()
 # Functionality here to check database for existing numbers   
 c.execute("SELECT * FROM factors WHERE number=?", [user_number])
 found_number = c.fetchall()
 
+# If user number is not found in database calculate prime factors
+#If use input is found from database print prime factors found by SQL query
 if found_number == []:
     # Store list of found prime factors in a variable
     prime_list = calc_prime_fact(user_number)
@@ -54,17 +60,27 @@ if found_number == []:
     # write prime factores to database file
     c.execute("INSERT INTO factors VALUES (:number, :factors)", {'number': user_number, 'factors': str_prime_list})
     con.commit()
-
+    # User output
     print("--------------------")
     print("Prime factors:")
     print(prime_list_output)
 else:
+    # New query for user input for formatting the output
     c.execute("SELECT * FROM factors WHERE number=?", [user_number])
+    # Show only column factors from table in the output
     new_number = c.fetchone()[1]
+    # User output
     print("--------------------")
     print("Prime factors:")
     print(new_number)
     print("This number form database")
+
+# End time of fetching prime factors
+end = time.time()
+exec_time = end - start
+# Show program execution time in output
+print("Time taken in calculating or fetching prime factors:")
+print(exec_time)
 
 
 
